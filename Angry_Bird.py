86c342772380c2e4e8f30b0f702f1cc648d19788
CFP104020010 <48315704+CFP104020010@users.noreply.github.com>
2019-03-16 14:18:33 +0800

    Add files via upload

---
+++ Angry_Bird.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[10]:
+
+
+import math
+import numpy as np
+import matplotlib.pyplot as plt
+pigx=np.random.random()*100
+pigy=np.random.random()*100
+v=0.1
+g=9.8
+deg=math.pi/4
+num=1000
+x=np.linspace(0, 1000, num, endpoint=True)
+y=math.tan(deg)*x-(g*x**2)/(2*v**2*math.cos(deg)**2)
+while pigy-(math.tan(deg)*pigx-(g*pigx**2)/(2*v**2*math.cos(deg)**2))>0.01:
+    v=v+0.01
+plt.plot(x, y, 'bo')
+plt.plot(pigx, pigy, 'ro')
+plt.show()
+        
+        
+    
+        
+   
+
+
+# In[ ]:
+
+
+
+
